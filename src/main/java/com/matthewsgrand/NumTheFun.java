38fe65529386e7a236363e7a57ee92c4d9f97f58
Matthew W Garvey <mwgarveyaa@gmail.com>
2020-08-04 14:33:34 -0500

    first commit

---
+++ src/main/java/com/matthewsgrand/NumTheFun.java
@@ -0,0 +1,17 @@
+package com.matthewsgrand;
+
+/**
+ * Hello world!
+ */
+public final class NumTheFun {
+    private NumTheFun() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Let's get started with Project 0.");
+    }
+}
